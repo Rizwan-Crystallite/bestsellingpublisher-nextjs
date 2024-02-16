9a6f5055b71f520da89b52ddd04ce56b9f34bee1
dev-mr <m.rizwan@crystal-lite.net>
2024-02-16 22:49:34 +0500

    Fix Csses

---
+++ src/fa-component/Bookshelf.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from '@/styles/Bookshelf.module.css'
+import { Container, Row, Col } from 'react-bootstrap'
+import Formdata from '@/fa-component/Formdata';
+// // mport { Fade } from "react-awesome-reveal";
+
+const Bookshelf = () => {
+    return (
+        <>
+            <section className={styles.bookshelfFlod}>
+                <Container fluid className='px-md-5'>
+                    <Row className={`${styles.bookshelfRow} justify-content-between`}>
+                        <Col xl={6}>
+                           
+                                <div className={styles.bookshelfTxt}>
+                                    <h4 className='font-24 fw-600 font-primary txt-primary mb-3'>Amazon or Any Platform of your Choice</h4>
+                                    <h2 className='font-50 fw-700 font-primary txt-secondary'>Comprehensive  <br className='d-none d-md-block' /> Book Publishing Services</h2>
+                                    <p className='font-15 font-secondary txt-secondary mb-3'>Best Selling Publisher offers a wide array of self-publishing services, for businesses and authors from all genres. We have a team of experienced professionals, guiding you through every step in the publishing process, from editing and formatting to publication, distribution and marketing. We also offer a variety of custom publishing options to meet your specifics, such as digital publishing services and Amazon publishing services.</p>
+
+                                    <p className='font-15 font-secondary txt-secondary'>Best Selling Publisher is a cutting-edge books publisher, offering a suite of services designed to help authors navigate the constantly changing world of publishing. Our book publishing services are tailored to meet the needs of today's authors. From providing guidance on self-publishing to offering and publishing support, we are committed to helping authors succeed in an increasingly competitive marketplace. Our commitment to quality sets Best Selling Publishers apart from all the other publishers in the market. We assist each author on an individual basis, providing the personalized attention that is essential for success in today's landscape of Publishing.</p>
+                                    <div className={`${styles.bttns} mt-5`}>
+                                        <Link className='btns btnPrimary' href="#">Let's discuss</Link>
+                                        <Link className='btns btnSecondary' href="tel:(800)781-9093">(800)781-9093</Link>
+                                    </div>
+                                </div>
+                            
+                        </Col>
+                        <Col xl={6}>
+                           
+
+                                <Formdata />
+                            
+
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </>
+    )
+}
+
+export default Bookshelf
---
+++ src/fa-component/Formdata.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import styles from '@/styles/Formdata.module.css'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import Router from 'next/router';
+
+const Formdata = () => {
+
+    const [score, setScore] = useState('Let`s discuss');
+
+    const handleSubmit = async (e) => {
+  
+      e.preventDefault()
+  
+      let bodyContent = JSON.stringify({
+        name: e.target.name.value,
+        email: e.target.email.value,
+        phone: e.target.phone.value,
+        message: e.target.comments.value,
+      });
+  
+      setScore('Wating For Sending Data');
+      let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: bodyContent
+      });
+  
+      let data = await response.text();
+      console.log(data);
+      e.target.reset();
+      setScore('Thank You');
+      const { pathname } = Router
+      if (pathname == pathname) {
+        Router.push('/thank-you')
+      }
+  
+    }
+    
+    return (
+        <>
+            <div className={styles.bookshelfForm}>
+                <Form className='mx-auto p-5' onSubmit={handleSubmit}>
+                    <h3 className='font-30 fw-700 font-primary txt-secondary text-center mb-5'>
+                        Get A <span className='txt-primary'>Free</span> Quote
+                    </h3>
+                    <Form.Group className="mb-4" controlId="name">
+                        <Form.Label className='font-13 font-secondary' name="name">Full Name *</Form.Label>
+                        <Form.Control type="text" required placeholder="Your Name" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-4" controlId="email">
+                        <Form.Label className='font-13 font-secondary' name="email">Email Address *</Form.Label>
+                        <Form.Control type="email" required placeholder="Type Email Address" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-4" controlId="phone">
+                        <Form.Label className='font-13 font-secondary' name="phone">Phone *</Form.Label>
+                        <Form.Control type="number" required placeholder="(123)-456-789" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-4" controlId="comments">
+                        <Form.Label className='font-13 font-secondary' name="comments">Comments</Form.Label>
+                        <Form.Control type="text" required placeholder="Message" />
+                    </Form.Group>
+
+                    <Button className='btns quoteBtns mt-3' type="submit"> {score} </Button>
+                </Form>
+            </div>
+
+        </>
+
+    )
+}
+
+export default Formdata
