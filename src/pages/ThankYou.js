b13ba94a2d45527ab493d76b3ad10fc68b0e3960
devsahal-crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2024-02-17 06:55:29 +0500

    form update

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -9,7 +9,7 @@ const ThankYou = () => {
       Thank You
       </h1>
       <p className='w-[60%] text-[16px] md:w-[70%] mx-auto mt-3 xs:w-[90%]'>
-      We've helped thousands of authors express themselves eloquently, and you may be the next! Hire our top-tier expert writers today and imprint your words in your readers' minds forever.
+      We've helped thousands of authors express themselves eloquently, and you may be the next! <br/> Hire our top-tier expert writers today and imprint your words in your readers' minds forever.
       </p>
       <div className='mx-auto py-2 xs:w-[90%] '>
             <div className='w-[30%] md:w-[60%] mx-auto grid grid-cols-2 grid-rows-1 gap-x-5 py-4 xs:w-[100%]'>
@@ -17,7 +17,7 @@ const ThankYou = () => {
               <a href="javascript:$zopim.livechat.window.show()" className='bg-[#1d1d1f] py-1 text-white border-solid border-[#1d1d1f] border-[1px] hover:bg-[#00C0E4] hover:text-[#fff] hover:border-[1px] hover:border-[#00C0E4] flex items-center justify-center text-center h-[40px]'>Live Chat</a>
             </div>
           </div>
-      <div className='mx-auto w-[100%] justify-center items-center text-center pb-20 xs:w-[90%] xs:pb-10'>
+      <div className='mx-auto w-[50%] justify-center items-center text-center pb-20 xs:w-[90%] xs:pb-10'>
         <img src='/assets/images/newimg/professional-ghostwriting.png' className='mx-auto' alt='professional-ghostwriting' />
       </div>
 
